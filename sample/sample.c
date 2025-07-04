6404a913a5de29aba040b47d343acba2f0026d4f
AnishSapre <anish.sapre654@gmail.com>
2025-07-04 10:19:56 +0530

    Initial commit

--- sample/sample.c
+++ sample/sample.c
@@ -1,4 +1,14 @@
-main()
+#include <stdio.h>
+
+#define LENGTH 10
+
+int main()
 {
-    
+#if defined(LENGTH)
+    printf("LENGTH is defined as %d\n", LENGTH);
+#else
+    printf("LENGTH is not defined\n");
+#endif
+
+    return 0;
 }
